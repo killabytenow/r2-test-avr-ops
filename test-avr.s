77c5fbc293fa32ccfea2ca719a9102dce7b1fba3
Gerardo García Peña <killabytenow@gmail.com>
2016-09-20 00:16:46 +0200

    more test

--- test-avr.s
+++ test-avr.s
@@ -1,4 +1,5 @@
-.INCLUDE "/usr/share/avra/m8def.inc"
+;.INCLUDE "def/m8def.inc"
+.INCLUDE "def/m32def.inc"
 
 .ORG 0x00
 reset:
@@ -34,31 +35,159 @@ main:
 	ldi	r16, LOW(RAMEND) 	; Lower byte
 	out	SPL, r16		; to stack pointer
 
+	; init r0 risc style
+	clr	r0
+
 	sei
 
 	; --------------------------------------------------
 	; TESTS
 	; --------------------------------------------------
 
-	rcall   test_ret
-	rcall   test_reti
+	rcall	test_ret
+.if FLASHEND > 0x0fff
+	call	test_ret
+.endif
+	rcall	test_reti
+	rcall	test_adc
+	rcall	test_bclr
 	rcall	test_cp
 
 	rjmp end
 
+; -----------------------------------------------------------------------------
+; RCALL/CALL/RET
+; -----------------------------------------------------------------------------
+
 test_ret:
 	ret
+
+; -----------------------------------------------------------------------------
+; RCALL/CALL/RETI
+; -----------------------------------------------------------------------------
 
 test_reti:
 	reti
 
+; -----------------------------------------------------------------------------
+; ADC
+; -----------------------------------------------------------------------------
+
+test_adc:
+	clr	r25
+	clr	r31
+
+	; ==> 0 + 0 + C(1) = 1
+	sec
+	ldi	r24, 0
+	ldi	r26, 0
+	ldi	r29, 0x80	; SREG (I)
+	ldi	r30, 0x01	; EXPECTED RESULT
+	adc	r24, r26
+	rcall	check_res
+
+	; ==> 0 + 0 + C(0) = 0
+	clc
+	ldi	r24, 0
+	ldi	r26, 0
+	ldi	r29, 0x82	; SREG (IZ)
+	ldi	r30, 0x00	; EXPECTED RESULT
+	adc	r24, r26
+	rcall	check_res
+
+	; ==> 100 + -1 + C(1) = 100
+	sec
+	ldi	r24, 100
+	ldi	r26, -1
+	ldi	r29, 0xa1	; SREG (IHC)
+	ldi	r30, 100	; EXPECTED RESULT
+	adc	r24, r26
+	rcall	check_res
+
+	; ==> 0 + -1 + C(1) = 0
+	sec
+	ldi	r24, 0
+	ldi	r26, -1
+	ldi	r29, 0xa1	; SREG (IHC)
+	ldi	r30, 0		; EXPECTED RESULT
+	adc	r24, r26
+	rcall	check_res
+
+	; ==> 0 + -1 + C(0) = 0
+	clc
+	ldi	r24, 0
+	ldi	r26, -1
+	ldi	r29, 0x94	; SREG (INV)
+	ldi	r30, -1		; EXPECTED RESULT
+	adc	r24, r26
+	rcall	check_res
+
+	ret;
+
+; -----------------------------------------------------------------------------
+; BCLR
+; -----------------------------------------------------------------------------
+
+test_bclr:
+	; clear result/expected result registers (r25:24, r31:30)
+	clr	r24
+	clr	r25
+	clr	r30
+	clr	r31
+
+	ldi	r16, 0xff
+	out	SREG, r16
+	ldi	r29, (0xff & ~0x1)
+	bclr	0
+	rcall	check_res
+	out	SREG, r29
+	ldi	r29, (0xff & ~0x3)
+	bclr	1
+	rcall	check_res
+	out	SREG, r29
+	ldi	r29, (0xff & ~0x7)
+	bclr	2
+	rcall	check_res
+	out	SREG, r29
+	ldi	r29, (0xff & ~0xf)
+	bclr	3
+	rcall	check_res
+	out	SREG, r29
+	ldi	r29, (0xff & ~0x1f)
+	bclr	4
+	rcall	check_res
+	out	SREG, r29
+	ldi	r29, (0xff & ~0x3f)
+	bclr	5
+	rcall	check_res
+	out	SREG, r29
+	ldi	r29, (0xff & ~0x7f)
+	bclr	6
+	rcall	check_res
+	out	SREG, r29
+	ldi	r29, (0xff & ~0xff)
+	bclr	7
+	rcall	check_res
+
+	sei
+	ret
+
+; -----------------------------------------------------------------------------
+; CP
+; -----------------------------------------------------------------------------
+
 test_cp:
+	clr	r25
+	clr	r31
+
 	; 5 == 5
 	ldi	r16, 5
 	ldi	r17, 5
-	ldi	r31, 0x02
+	ldi	r30, 5
+	ldi	r31, 0x82
+; FAIL
 	cp	r16, r17
-	rcall	check_sreg
+	rcall	check_res
 
 	; 5 == 6
 	ldi	r17, 6
@@ -70,14 +199,25 @@ test_cp:
 
 	ret
 
-check_sreg:
+check_res:
+	; check sreg (flags cf, zf, of, ...)
 	push	r1
 	in	r1, SREG
-	cp	r1, r31
+	cp	r1, r29
 	pop	r1
 	brne	fail
-	clr	r1
-	out	SREG, r1
+
+	; clear SREG
+	out	SREG, r0
+	sei
+
+	; check result (r25:r24 == r31:r30)
+	cp	r25, r31
+	brne	fail
+	cp	r24, r30
+	brne	fail
+
+	; ok
 	ret
 
 end:
